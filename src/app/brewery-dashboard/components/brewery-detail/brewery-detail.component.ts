3d6d6de38401a2c8af2a3aa7f8e07d9441b8fa22
David Reese <dave@freshconsulting.com>
2018-09-03 22:48:36 -0700

    don't link to map or directions if there are no coordinates

--- src/app/brewery-dashboard/components/brewery-detail/brewery-detail.component.ts
+++ src/app/brewery-dashboard/components/brewery-detail/brewery-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, Output } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 import { Brewery } from '../../models/brewery.interface';
 
@@ -8,11 +8,14 @@ import { Brewery } from '../../models/brewery.interface';
   template: `
     <div class="brewery-detail">
       <span class="brewery-detail__number">{{ 1 + index }}</span>
-      <h2 class="brewery-detail__title"><a [routerLink]="'/map/'+detail.$key">{{ detail.name }}</a></h2>
+      <h2 class="brewery-detail__title">
+        <a [routerLink]="'/map/'+detail.$key" *ngIf="detail.latitude && detail.longitude">{{ detail.name }}</a>
+        <span *ngIf="!detail.latitude && !detail.longitude">{{ detail.name }}</span>
+      </h2>
       <p>{{ detail.address }}<br>
       {{ detail.city }} WA<span *ngIf="detail.zip">,</span> {{ detail.zip }}</p>
       <tag *ngFor="let tag of tags" [tag]="tag"></tag>
-      <p class="brewery-detail__meta"><a href="{{ detail.url }}" target="_blank">Website</a> | <a href="https://www.google.com/maps/dir//{{detail.address}},{{detail.city}},WA,{{detail.zip}}" target="_blank">Directions</a></p>
+      <p class="brewery-detail__meta"><a href="{{ detail.url }}" target="_blank">Website</a><span *ngIf="detail.latitude && detail.longitude"> | <a href="https://www.google.com/maps/dir//{{detail.address}},{{detail.city}},WA,{{detail.zip}}" target="_blank">Directions</a></span></p>
     </div>
   `
 })
